9d5dc97d30fe0dcbdad903faa260c94517db9cf1
pAplakidis <aplakidisp@gmail.com>
2023-07-11 09:13:16 +0300

    basic Carla script

---
+++ controls/path_planner.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+import numpy as np
+
+
+# TODO: get the model's output trajectories as inputs (sort by probablity)
+# TODO: smoothen out the path curve
+
+if __name__ == "__main__":
+    print("Hello")
