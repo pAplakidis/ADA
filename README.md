34bd940836a70d78bf83ac1ec6f240d5bc57aabd
pAplakidis <aplakidisp@gmail.com>
2023-08-25 09:46:10 +0300

    plannerd can get desire

--- README.md
+++ README.md
@@ -25,12 +25,13 @@ In three terminals run the following in each:
 ### TODO:
 - cannot work longterm, need to handle memory better
 - ROS messaging (DONE)
+- log all ROS messages to a file instead of stdout
 - camera daemon to feed images  (DONE)
 - add try/except for every ROS message published and received
-- sensors daemon to feed sensor data (desires for now)
+- sensors daemon to feed sensor data (desires for now)  (DONE)
 - model daemon to process that image and output stuff (DONE)
 - fps and other metrics for performance monitoring
-- modeld.cc for faster model processing
+- modeld.cc and .onnx for faster model processing
 - planner (longitudinal and lateral) daemon that preprocesses the path and feeds it to the controller
 - controls daemon that decides the overall controls from a path using PID
 - board daemon that handles the embedded system ADA runs on
--- plannerd.py
+++ plannerd.py
@@ -6,22 +6,26 @@ from std_msgs.msg import Float64MultiArray
 
 # TODO: smoothen out the path curve, or wait for it to be mean,std,prob instead of xy_points,prob
 class LateralPlanner:
-  def __init__(self, verbose=False, n_modes=3, trajectory_length=200, n_coords=2):
+  def __init__(self, verbose=False, trajectory_length=200, n_coords=2):
     self.verbose = verbose
-    self.n_modes = n_modes
     self.trajectory_length = trajectory_length
     self.n_coords = n_coords
 
+    self.subscriber = rospy.Subscriber("/sensor/desire", Float64MultiArray, self._desire_callback)
+
     self.desire = np.array([1, 0, 0]) # forward as default
-    self.path_probs = np.zeros(n_modes)
     self.xy_path = np.zeros((trajectory_length, n_coords))
 
   def run_inference_loop(self):
     while not rospy.is_shutdown():
       rospy.spin()
 
+  def _desire_callback(self, msg):
+    self.desire = msg.data
+    if self.verbose:
+      print("[plannerd]: desire ->", self.desire)
+
   def update(self, model_outputs):
-    # TODO: get desire
     self.xy_path= np.array(model_outputs[:-1]).reshape((self.trajectory_length, self.n_coords))
     self.crossroad = model_outputs[-1]
     if self.verbose:
@@ -37,11 +41,11 @@ class LongitudinalPlanner:
 
 
 class Plannerd:
-  def __init__(self):
+  def __init__(self, verbose=False):
     self.subscriber = rospy.Subscriber("/model/outputs", Float64MultiArray, self.update_lateral)
     # TODO: publisher -> /planner/trajectory, etc
 
-    self.lateral_planner = LateralPlanner()
+    self.lateral_planner = LateralPlanner(verbose=verbose)
     self.longitudinal_planner = LongitudinalPlanner()
 
   def update_lateral(self, msg):
